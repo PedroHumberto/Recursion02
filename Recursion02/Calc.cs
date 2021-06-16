393e1d10c3f49c590e575cd2e12361acf772b681
PedroHumberto <pedrohlucape@yahoo.com.br>
2021-06-16 20:47:52 -0300

    Add project files.

---
+++ Recursion02/Calc.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion02
+{
+    class Calc
+    {
+        public int fat(int n)
+        {
+            int res;
+            if (n <= 1)
+            {
+                res = 1;
+            }
+            else
+            {
+                res = n * fat(n - 1);
+            }
+            return res;
+
+
+        }
+    }
+}
---
+++ Recursion02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace Recursion02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Calc calc = new Calc();
+
+            int res = calc.fat(5);
+
+            Console.WriteLine(res);
+
+
+        }
+    }
+}
