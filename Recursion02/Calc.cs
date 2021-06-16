bdaa896ebdd68410c5a88b4fcc9dfe10d7b6de2c
PedroHumberto <pedrohlucape@yahoo.com.br>
2021-06-16 20:50:00 -0300

    Corrections1

--- Recursion02/Calc.cs
+++ Recursion02/Calc.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Recursion02
+﻿namespace Recursion02
 {
     class Calc
     {
